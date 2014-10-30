4645a7ace13ee8782be8ded035a99321e59fe791
Hersh Sanghvi <hersh500@gmail.com>
2014-10-30 13:38:24 -0700

    Introduction to Basic Math ops and Vars

---
+++ Intro_Programs/math_and_vars.py
@@ -0,0 +1,17 @@
+#This program demonstrates the use of variables
+#Variables can be used to store data of many types
+
+v = 1 #This variable is an integer
+print("The value of v is now", v)
+v = v + 1 #This modifies the value of the variable
+
+print("V is now", v) #What will this output?
+
+x = 0.5 #This variable is a "float" 
+x = x + 45
+print("The value of x is now", x) 
+
+#To multiply x by 5, and store that in another variable, you would write...
+
+y = x * 5
+
